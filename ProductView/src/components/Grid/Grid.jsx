e99995d6bb4a53987a644972d705b5d11c2536ab
AyushJha2008 <ayushsjha2008@gmail.com>
2025-07-04 19:21:46 +0530

    api response and error

--- ProductView/src/components/Grid/Grid.jsx
+++ ProductView/src/components/Grid/Grid.jsx
@@ -1,8 +1,39 @@
-import React from 'react'
+import { useEffect } from "react";
+import { useState } from "react";
+import './grid.css'
 
 const Grid = () => {
+    const [products, setproducts] = useState([]);
+
+    useEffect(()=>{
+        fetch('https://dummyjson.com/products?limit=30')
+        .then(response => response.json())
+        .then(data => setproducts(data.products))
+        .catch(err => console.error('error fetching data:',err))
+    },[]);
+    console.log(products);
+
   return (
-    <div>Grid</div>
+    <div className='grid'>
+        <div className="header"><h2>Tap to see more info</h2></div>
+        <div className="product-grid">
+            {products.map(product =>(
+                <div className="product" key={product.id}>
+                    <img src={product.thumbnail} alt={product.title} />
+                    <div className="product-head">
+                        <h3>{product.title}</h3>
+                    </div>
+                    <div>
+                        <span>{product.rating}⭐</span>
+                        <span>{product.stock}</span>
+                    </div>
+                    <div className="product-price">
+                        <span>$ {product.price}</span>
+                    </div>
+                </div>
+            ))}
+        </div>
+    </div>
   )
 }
 
--- ProductView/src/pages/Home/Home.jsx
+++ ProductView/src/pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from '../../components/Navbar/Navbar'
+import Grid from '../../components/Grid/Grid'
 
 const Home = () => {
   return (
     <div>
         <Navbar/>
+        <Grid/>
     </div>
   )
 }

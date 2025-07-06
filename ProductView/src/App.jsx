00fce8b810511ea5ba2f3cc270cb4c4eb8a5c9b5
AyushJha2008 <ayushsjha2008@gmail.com>
2025-07-06 20:14:23 +0530

    preview and readme

--- ProductView/src/App.jsx
+++ ProductView/src/App.jsx
@@ -3,7 +3,6 @@ import {Route, Routes} from 'react-router-dom'
 import Home from '../../ProductView/src/pages/Home/Home'
 import Login from '../../ProductView/src/pages/Login/Login'
 import Product from '../../ProductView/src/pages/product/Product'
-import './App.css'
 
 const App = () => {
 
--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# Ace360-Assignment
+-## How to Run:
+
+- cd ProductView
+- npm run dev
+
+# Preview
+
+![Output Image](ProductView/src/assets/output1.png)
+![Output Image](ProductView/src/assets/output2.png)
+![Output Image](ProductView/src/assets/output3.png)
